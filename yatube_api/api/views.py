67e8fa2df9f802a8958086bf227456cfe2257753
Alex <ugabuga.gmail.com>
2026-04-28 23:46:09 +0300

    Fix PEP8 violations: remove unused imports

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import permissions, viewsets
 
-from posts.models import Comment, Group, Post
+from posts.models import Group, Post
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 
 
